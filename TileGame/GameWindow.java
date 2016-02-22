6e7cd5692edc0d12cce91163c70e957b5109e382
tbrutsm1 <tbrutsm1@uwyo.edu>
2016-02-21 17:46:52 -0700

    Reworked the GameWindow.java and Main.java code and added a Tile.java class

---
+++ TileGame/GameWindow.java
@@ -0,0 +1,181 @@
+package TileGame;
+
+import java.util.*;
+import java.util.List;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import javax.swing.ImageIcon;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameWindow implements ActionListener {
+    
+    public static final int BEVEL = 10;
+    public static final Dimension PANEL_SIZE =
+            new Dimension(700, 700);
+    public static final String TILE_IMAGE = "TileImages/";
+    public final List<Rectangle> freeSpots = new ArrayList<Rectangle>(16);
+    private JPanel panel;
+    private JFrame window;
+    protected static JButton newGameButton, resetButton, quitButton;
+
+    /**
+     * Create the GUI objects and display the game
+     */
+    public  GameWindow() {
+        // Create the window and panel
+        window = new JFrame("Group F aMaze");
+        panel = new JPanel(null);
+        // Call to create a new game
+        newGame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setBackground(Color.cyan);
+        window.setResizable(false);
+        window.setVisible(true);
+        window.add(panel);
+        window.pack();
+        
+    }
+    
+    /**
+     * Clear the JPanel and freeSpots list and set up a new game
+     */
+    private void newGame() {
+        //clear the board, clear the free spots
+        //create a new deck of cards and shuffle it
+        panel.removeAll();
+        freeSpots.clear();
+      
+        
+        Rectangle newGameBounds = new Rectangle(50, 50, 100, 50);
+        newGameButton = new JButton("New Game");
+        newGameButton.setVerticalTextPosition(AbstractButton.CENTER);
+		newGameButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		newGameButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					 newGame();
+				}          
+			}
+		);
+		newGameButton.setBounds(newGameBounds);
+		panel.add(newGameButton);
+        
+		Rectangle resetBounds = new Rectangle(160, 50, 100, 50);
+        resetButton = new JButton("Reset");
+        resetButton.setVerticalTextPosition(AbstractButton.CENTER);
+		resetButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		resetButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					 reset();
+				}          
+			}
+		);
+		resetButton.setBounds(resetBounds);
+		panel.add(resetButton);
+		
+		Rectangle quitBounds = new Rectangle(270, 50, 100, 50);
+        quitButton = new JButton("Quit");
+        quitButton.setVerticalTextPosition(AbstractButton.CENTER);
+		quitButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		quitButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					 window.dispose();				}          
+			}
+		);
+		quitButton.setBounds(quitBounds);
+		panel.add(quitButton);
+		
+		
+        //shuffleLimit = 2;
+        List<Tile> tileList = new ArrayList<Tile>(16);
+        for (int i = 1; i <= 16; i++){
+            tileList.add(new Tile(i, this));
+        }
+        
+        Collections.shuffle(tileList);
+        
+        
+        // Paint the 16 free spots in the center as red
+        for (int i = 0; i < 16; i++) {            
+            Rectangle bounds = new Rectangle(215 +(i/4)*(Tile.TILE_W+BEVEL),
+                                             275 + BEVEL+(i%4)*(Tile.TILE_W+BEVEL),
+                                             Tile.TILE_W, Tile.TILE_W);
+            freeSpots.add(bounds);
+            JLabel emptyTile = new JLabel(new ImageIcon(TILE_IMAGE+"red.gif"));
+            emptyTile.setBounds(bounds);
+            panel.add(emptyTile);
+        }
+        for (int j = 0; j < 16; j++)
+        {
+        	if(j < 4)
+        	{
+        		Rectangle tileBounds = new Rectangle(215 +(j)*(Tile.TILE_W+BEVEL), 
+        											 165, 
+        											 Tile.TILE_W, 
+        											 Tile.TILE_W);
+
+            	tileList.get(j).setBounds(tileBounds);
+        	}
+        	else if(j < 8)
+        	{
+        		Rectangle tileBounds  = new Rectangle(550, 275 + BEVEL+(j-4)*(Tile.TILE_W+BEVEL), 
+													 Tile.TILE_W, 
+													 Tile.TILE_W);
+
+            	tileList.get(j).setBounds(tileBounds);
+        	}
+        	else if(j < 12)
+        	{
+        		Rectangle tileBounds = new Rectangle(215 +(j-8)*(Tile.TILE_W+BEVEL), 
+						 615, 
+						 Tile.TILE_W, 
+						 Tile.TILE_W);
+
+        		tileList.get(j).setBounds(tileBounds);
+        	}
+        	else
+        	{
+        		Rectangle tileBounds  = new Rectangle(90, 275 + BEVEL+(j-12)*(Tile.TILE_W+BEVEL), 
+						 Tile.TILE_W, 
+						 Tile.TILE_W);
+
+        		tileList.get(j).setBounds(tileBounds);
+        	}
+    		panel.add(tileList.get(j));
+        	
+        }
+        panel.setPreferredSize(PANEL_SIZE);   
+        panel.setBackground(Color.cyan);
+        panel.repaint();        
+    }
+    
+    /**
+     * resets the game board when reset button is clicked
+     */
+    private void reset() { 
+    	// not finished
+    }
+    
+    /**
+     * tests if the game has been won
+     * @return true if the game has been won, else returns false
+     */
+    public boolean isGameWon() {
+    	// Will check that each tile is in its correct position
+	    // Game must be won if we reach this return statement
+    	
+	    //return true;
+    	// not finished
+    	return false;
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}  
+}
---
+++ TileGame/Tile.java
@@ -0,0 +1,88 @@
+package TileGame;
+
+import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class Tile extends JLabel {
+
+    public static final int TILE_W = 60;    //square tile image width
+    public int position;            		//the numeric rank of the tile position
+    private GameWindow gameWin;          	//GameWindow object in which this tile is part of
+    
+    public Tile(int pos, GameWindow gameW){
+    	//BufferedImage bufImg = ImageIO.read(new File(TILE_IMAGE + "hexagonPattern.jpg");
+        super(new ImageIcon(GameWindow.TILE_IMAGE+"gold.gif"));
+        MouseHandler listener = new MouseHandler();
+        this.addMouseListener(listener);        //add mouseListeners to the tile
+        this.addMouseMotionListener(listener);
+        position = pos;
+        gameWin = gameW;
+    }
+    
+    private class MouseHandler extends MouseInputAdapter {
+        private int clickDisX;      //displacement when clicking on the Tile
+        private int clickDisY;
+        private Point origin;       //the point where the Tile is located
+        public void mouseDragged(MouseEvent e) {
+            Tile Tile = (Tile) e.getComponent();
+            JPanel panel = (JPanel) Tile.getParent();
+            //here we check that the Tile is not dragged off the panel
+            //by resetting the Tile's coordinates if it starts to move off
+            int newX = Math.max(Tile.getX() + e.getX() - clickDisX, 0);
+            newX = Math.min(newX, panel.getWidth() - TILE_W);
+            int newY = Math.max(Tile.getY() + e.getY() - clickDisY, 0);
+            newY = Math.min(newY, panel.getHeight() - TILE_W);
+            //update the Tile's new locations
+            Tile.setLocation(newX, newY);
+        }
+        public void mousePressed(MouseEvent e) {
+            Tile Tile = (Tile) e.getComponent();
+            JPanel panel = (JPanel) Tile.getParent();
+            //save the original location of the Tile
+            origin = Tile.getLocation();
+            clickDisX = e.getX();
+            clickDisY = e.getY();
+            //move the Tile to the top of the panel
+            //and repaint it so it appears above all others
+            panel.setComponentZOrder(Tile, 0);
+            panel.repaint();
+        }
+        public void mouseReleased(MouseEvent e) {
+            Tile Tile = (Tile) e.getComponent();
+            JPanel panel = (JPanel) Tile.getParent();
+            //assume the move is not legal
+            boolean legalRelease = false;
+            //make sure the mouse is release inside the JPanel
+            if(panel.getMousePosition() != null){
+                //search all the free spots for this mouse coordinate
+                for (Rectangle free : gameWin.freeSpots) {
+                    //if we have found the correct free spot...
+                    if(free.contains(panel.getMousePosition())) {
+                        //update the Tile's location to the free spot
+                        Tile.setLocation(free.getLocation());
+                        //update free spot's location to the Tile's origin
+                        free.setLocation(origin);
+                        //final Point move = free.getLocation();
+                        //set flag to show a valid move
+                        legalRelease = true;
+                    }
+                }
+            }
+            // if the move was not valid, snap back to original spot and beep
+            if(!legalRelease) {
+                Tile.setLocation(origin);
+                java.awt.Toolkit.getDefaultToolkit().beep();
+            }
+            
+            // ask the game window if the game was won
+            if(gameWin.isGameWon()) {
+                JOptionPane.showMessageDialog(panel.getParent(),
+                        "You Win");
+            }
+        }
+    }
+}
+
+

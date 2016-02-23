7b2218845e238944aba47469c7494d55e5ff78e6
larfylarf <chris_ruiz@live.com>
2016-02-22 19:01:14 -0700

    Revert "Changed Color"
    This reverts commit d96c119f014e3a964d433bfdae61df4484a69497.
    
    Restoring .java files before last commit.

---
+++ TileGame/Main.java
@@ -0,0 +1,12 @@
+package TileGame;
+
+public class Main {
+
+    /**
+     * Creates the new Game object which it then runs
+     * @param args (ignored)
+     */
+    public static void main(String[] args) {
+        new GameWindow();        
+    }
+}
